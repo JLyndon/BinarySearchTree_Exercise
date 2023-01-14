930e6b0b6c4f46779a523a251b3a05dcabe74140
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2023-01-14 12:09:00 +0800

    Implemented In order traversal Function

--- BST_base.py
+++ BST_base.py
@@ -1,3 +1,5 @@
+import random as rand
+
 class BinarySearchTreeNode:
     def __init__(self, data):
         self.data = data
@@ -18,3 +20,34 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visit base node
+        elements.append(self.data)
+
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+    
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    array = [17, 4, 1, 20, 9, 23, 18, 34]
+
+    numbers_tree = build_tree(array)
+
+    print(numbers_tree.in_order_traversal())
